d4bd2143539661d8b6e6e2d6251c5efa8e1f5316
Hieu Tran <hieutran1985@gmail.com>
2019-05-27 11:20:44 -0400

    merge step_4_2

--- aa_workflow_step_4_2.py
+++ aa_workflow_step_4_2.py
@@ -25,7 +25,7 @@ def change_I_to_L(string):
     return string.replace('I', 'L')
 
 
-def step_4_2(denovo_file, db_fasta_file, labeled_feature_file, peptide_list_fasta):
+def preprocess(denovo_file, db_fasta_file, labeled_feature_file, peptide_list_fasta):
   """Remove denovo peptides that exist in the database fasta file.
      Combine db and denovo into a peptide list file for PEAKS X DB search round 2.
 
@@ -37,7 +37,7 @@ def step_4_2(denovo_file, db_fasta_file, labeled_feature_file, peptide_list_fast
   """
 
   print("".join(["="] * 80)) # section-separating line
-  print("step_4_2()")
+  print("preprocess()")
 
   print("denovo_file =", denovo_file)
   print("db_fasta_file =", db_fasta_file)
@@ -104,4 +104,35 @@ def step_4_2(denovo_file, db_fasta_file, labeled_feature_file, peptide_list_fast
   num_db_peptides = len(db_peptide_set)
   num_denovo_peptides = len(pure_denovo_seq_set)
   print("num_db_peptides =", num_db_peptides)
+  print("num_denovo_peptides =", num_denovo_peptides)
+
+
+def postprocess(psm_file, output_denovo_peptide_file):
+  """Extract denovo peptides from the PSMs of PEAKS X DB search round 2.
+
+     Usage:
+       psm_file = "data.training/aa.hla.bassani.nature_2016.mel_16.class_1/aa_workflow.step_4.psm.csv"
+       output_denovo_peptide_file = "data.training/aa.hla.bassani.nature_2016.mel_16.class_1/aa_workflow.step_4.output_peptide_list"
+  """
+
+  print("".join(["="] * 80)) # section-separating line
+  print("postprocess()")
+
+  print("psm_file =", psm_file)
+  print("output_denovo_peptide_file =", output_denovo_peptide_file)
+
+  denovo_peptide_set = set()
+  with open(psm_file, 'r') as input_handle:
+    csv_reader = csv.DictReader(input_handle, delimiter=',')
+    for row in csv_reader:
+      peptide = drop_mod_peaks(row['Peptide'])
+      accession = drop_mod_peaks(row['Accession'])
+      if accession == 'DENOVO':
+        denovo_peptide_set.add(peptide)
+
+  with open(output_denovo_peptide_file, 'w') as output_handle:
+    for peptide in denovo_peptide_set:
+        output_handle.write(peptide + '\n')
+
+  num_denovo_peptides = len(denovo_peptide_set)
   print("num_denovo_peptides =", num_denovo_peptides)
